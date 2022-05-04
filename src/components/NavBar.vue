897363d63162471dd7cdc932fe3fd168b939aa64
Sami-21 <samovitch360@gmail.com>
2022-05-04 19:38:29 +0100

    NavBar done

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="w-screen h-32 z-30 flex justify-between items-center px-16 fixed text-white top-0 left-0"
+    class="w-screen h-32 z-10 flex justify-between items-center px-16 fixed text-white top-0 left-0"
   >
     <div class="w-48 text-center mr-8">
       <div class="w-full flex justify-center align-center">
@@ -32,7 +32,7 @@
 
     <ul
       id="NavItems"
-      class="md:flex md:h-full md:relative md:top-0 md:left-0 flex-col md:flex-row justify-between items-center lg:text-3xl text-xl absolute -top-60 left-0 text-center h-60 py-4 z-0"
+      class="md:flex md:h-full md:relative md:top-0 md:left-0 flex-col md:flex-row justify-between items-center lg:text-3xl text-xl absolute top-32 -left-full text-center md:opacity-100 opacity-0 h-60 py-4 z-10 md:border-t-0 border-t-2"
     >
       <li>HOME</li>
       <li>PRODUCT</li>
@@ -41,13 +41,23 @@
       <li>CONTACT</li>
     </ul>
 
-    <div class="pl-20 text-center">
-      <v-icon dark size="52px">mdi-magnify</v-icon>
+    <div class="pl-28 text-center">
+      <v-icon @click="toggleSearchBar()" dark size="52px">mdi-magnify</v-icon>
     </div>
     <div @click="toggleMenu()" id="NavBarIcon" class="cursor-pointer md:hidden">
-      <div class="h-1 w-8 bg-white my-2 duration-300"></div>
-      <div class="h-1 w-8 bg-white my-2 duration-300"></div>
-      <div class="h-1 w-8 bg-white my-2 duration-300"></div>
+      <div class="bar1 h-1 w-9 bg-white my-2 duration-300"></div>
+      <div class="bar2 h-1 w-9 bg-white my-2 duration-300"></div>
+      <div class="bar3 h-1 w-9 bg-white my-2 duration-300"></div>
+    </div>
+    <div
+      id="searchContainer"
+      class="flex justify-center items-center w-full h-0 border-t-2 absolute top-32 left-0"
+    >
+      <input
+        type="text"
+        placeholder="search..."
+        class="bg-gray-200 w-full md:mx-32 mx-12 outline-none p-2 rounded text-black"
+      />
     </div>
   </nav>
 </template>
@@ -55,15 +65,34 @@
 <script>
 export default {
   data: () => ({
-    NavBarItems: ["test1", "test2", "test3", "test4", "test5"],
     MenuClosed: true,
+    SearchbarClosed: true,
   }),
   methods: {
     toggleMenu() {
-      document.querySelector("#NavItems").classList.toggle("top-32");
-      document.querySelector("#NavItems").classList.toggle("-top-60");
-
-      console.log(document.querySelector("#NavBarIcon").childNodes);
+      document
+        .querySelector("#NavItems")
+        .classList.toggle("NavItemsTransition");
+      document.querySelector("#NavItems").classList.toggle("-left-full");
+      document.querySelector("#NavItems").classList.toggle("left-0");
+      document.querySelector("#NavItems").classList.toggle("opacity-0");
+      document.querySelector("#NavItems").classList.toggle("opacity-100");
+      document.querySelector(".bar1").classList.toggle("bar1_translate");
+      document.querySelector(".bar2").classList.toggle("opacity-0");
+      document.querySelector(".bar3").classList.toggle("bar2_translate");
+      this.menuClosed = !this.menuClosed;
+    },
+    toggleSearchBar() {
+      document.querySelector("#searchContainer").classList.toggle("h-0");
+      document.querySelector("#searchContainer").classList.toggle("h-1/2");
+      this.searchBarClosed = !this.searchBarClosed;
+      if (this.searchBarClosed) {
+        document.querySelector("#NavItems").classList.add("top-48");
+        document.querySelector("#NavItems").classList.remove("top-32");
+      } else {
+        document.querySelector("#NavItems").classList.remove("top-48");
+        document.querySelector("#NavItems").classList.add("top-32");
+      }
     },
   },
 };
@@ -76,7 +105,20 @@ nav {
 #NavItems {
   width: 750px;
   list-style: none;
-  transition: 500ms;
+  transition-property: top opacity;
+  transition-duration: 500ms;
+}
+
+#searchContainer {
+  background: #2c2c2c;
+  overflow: hidden;
+  transition: height 500ms;
+}
+.bar1_translate {
+  transform: rotate(-45deg) translate(-9px, 6px);
+}
+.bar2_translate {
+  transform: rotate(45deg) translate(-9px, -9px);
 }
 @media (max-width: 768px) {
   #NavItems {
